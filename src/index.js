aa43eedd97f88630a0b50d110264b38b970a78e9
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-10-05 16:00:14 +0530

    some modification

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider as ReduxProvider } from "react-redux";
-import { store } from "../src/Components/store/store";
+import { store } from "../src/store/store";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
